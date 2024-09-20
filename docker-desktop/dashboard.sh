db387c97ca0b01ee25372425e0d515a00b7a3edc
abs2free <abs2free@gmail.com>
2024-09-20 12:59:44 +0800

    up

--- docker-desktop/dashboard.sh
+++ docker-desktop/dashboard.sh
@@ -1,8 +1,3 @@
-helm upgrade --install ingress-nginx ingress-nginx \
-  --repo https://kubernetes.github.io/ingress-nginx \
-  --namespace ingress-nginx --create-namespace
-
-
 # Add kubernetes-dashboard repository
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 # Deploy a Helm Release named "kubernetes-dashboard" using the kubernetes-dashboard chart
